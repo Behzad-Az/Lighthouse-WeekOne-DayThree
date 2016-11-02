7c86aea6ff913eb70544f1c3a94c5f0e9d5d95d5
Behzad-Az <behy1368@gmail.com>
2016-11-02 18:08:49 +0000

    committing working version of countLetters exercise

---
+++ countLetters.js
@@ -0,0 +1,25 @@
+//Get arguments as string and remove spaces.
+var arg = process.argv[2].split(' ').join('');
+
+//Declare empty object.
+var letterObj = {};
+
+//Return number of each letter in argument.
+console.log(countLetters(arg));
+
+//Count number of each letter.
+function countLetters(txt) {
+  for (var i = 0; i < txt.length; i++) {
+
+    //If it's the first time observing the ltter, initilize the
+    //corresponding object property value from undefined to 0.
+    if (letterObj[txt[i]] === undefined) {letterObj[txt[i]] = 0;}
+
+    //Add one to the corresponding object property value.
+    letterObj[txt[i]] += 1;
+  }
+  //return object.
+  return letterObj;
+}
+
+
